8cc13dfe13cd752021f653c8ece7eab105734532
Kirill Trofimov <crazytraffic@gmail.com>
2017-12-16 18:11:07 +0500

    clean code day 16

--- day 16/solution.py
+++ day 16/solution.py
@@ -1,19 +1,20 @@
 import re
-dance = list("abcdefghijklmnop")
-f = open("input").read().strip().split(",")
 
-for line in f:
-    if line[0] == "s":
-        offset = int(line[1:])
-        dance = dance[-offset:]+dance[:-offset]
-    if line[0] == "x":
-        temp = re.match("x(\d+)/(\d+)",line)
-        pos1 = int(temp[1])
-        pos2 = int(temp[2])
-        dance[pos1], dance[pos2] = dance[pos2], dance[pos1]
-    if line[0] == "p":
-        pos1 = dance.index(line[1])
-        pos2 = dance.index(line[3])
-        dance[pos1], dance[pos2] = dance[pos2], dance[pos1]
+def get_dance(text, moves):
+    dance = list(text)
+    for line in moves:
+        if line[0] == "s":
+            offset = int(line[1:])
+            dance = dance[-offset:]+dance[:-offset]
+        elif line[0] == "x":
+            temp = re.match("x(\d+)/(\d+)",line)
+            pos1, pos2 = int(temp[1]), int(temp[2])
+            dance[pos1], dance[pos2] = dance[pos2], dance[pos1]
+        elif line[0] == "p":
+            pos1, pos2  = dance.index(line[1]), dance.index(line[3])
+            dance[pos1], dance[pos2] = dance[pos2], dance[pos1]
+    return "".join(dance)
 
-print ("".join(dance))
+
+move_list = open("input").read().strip().split(",")
+print(get_dance("abcdefghijklmnop", move_list))
--- day 16/solution_2.py
+++ day 16/solution_2.py
@@ -1,42 +1,33 @@
 import re
-dance = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p']
-f = open("input").read().strip().split(",")
-unique = {"abcdefghijklmnop":0}
-moves = []
-for line in f:
-    if line[0] == "s":
-        offset = int(line[1:])
-        moves.append(("s",offset))
-    if line[0] == "x":
-        temp = re.match("x(\d+)/(\d+)",line)
-        pos1 = int(temp[1])
-        pos2 = int(temp[2])
-        moves.append(("x",pos1,pos2))
-    if line[0] == "p":
-        moves.append(("p",line[1],line[3]))
-i = 0
-rrr = 1000000000
-print(len(moves),moves)
-while i < rrr:
-    for x in moves:
-        if x[0] == "s":
-            dance = dance[-x[1]:]+dance[:-x[1]]
-        if x[0] == "x":
-            dance[x[1]], dance[x[2]] = dance[x[2]], dance[x[1]]
-        if x[0] == "p":
-            pos1 = dance.index(x[1])
-            pos2 = dance.index(x[2])
-            dance[pos1], dance[pos2] = dance[pos2], dance[pos1]
-    i += 1
-    string = "".join(dance)
-    if string in unique:
-        rrr = 1000000000 % (i-unique[string])
-        i = 0
-        dance = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p']
-        print("find cycle!",string,)
-        unique = {"abcdefghijklmnop":0}
-        print(unique)
-    else:
-        unique[string] = i
 
-print ("".join(dance))
+def get_dance(text, moves):
+    dance = list(text)
+    for line in moves:
+        if line[0] == "s":
+            offset = int(line[1:])
+            dance = dance[-offset:]+dance[:-offset]
+        elif line[0] == "x":
+            temp = re.match("x(\d+)/(\d+)",line)
+            pos1, pos2 = int(temp[1]), int(temp[2])
+            dance[pos1], dance[pos2] = dance[pos2], dance[pos1]
+        elif line[0] == "p":
+            pos1, pos2  = dance.index(line[1]), dance.index(line[3])
+            dance[pos1], dance[pos2] = dance[pos2], dance[pos1]
+    return "".join(dance)
+
+def get_cycle_count(text, moves):
+    unique = {}
+    i = 0
+    while not text in unique:
+        unique[text] = i
+        text = get_dance(text, moves)
+        i += 1
+    return 1000000000 % (i - unique[text])
+
+
+move_list = open("input").read().strip().split(",")
+text = "abcdefghijklmnop"
+count = get_cycle_count(text, move_list)
+for i in range(count):
+    text = get_dance(text, move_list)
+print (text)
