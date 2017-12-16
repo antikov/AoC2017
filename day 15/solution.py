34ff80a70610b6d5b4d206f8b8e0d44050bf7c26
Kirill Trofimov <crazytraffic@gmail.com>
2017-12-16 18:25:30 +0500

    clean code day 15

--- day 15/solution.py
+++ day 15/solution.py
@@ -1,15 +1,14 @@
-a = 116
-b = 299
+import re
+
+f = open("input").read().strip().split("\n")
+a = int(re.match("Generator A starts with (\d+)",f[0])[1])
+b = int(re.match("Generator B starts with (\d+)",f[1])[1])
+
 mask = 2**16 - 1
 count = 0
-for i in range(5000000):
+for i in range(40000000):
     a = (a * 16807) % 2147483647
     b = (b * 48271) % 2147483647
-    while (a & -a) < 4:
-        a = (a * 16807) % 2147483647
-    while (b & -b) < 8:
-        b = (b * 48271) % 2147483647
-
     if a & mask == b & mask:
         count += 1
 
